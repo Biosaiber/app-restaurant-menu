d4ae9be72d69ed585ebefb0e2340626d063f726b
isubfox <samuel.liska@gmail.com>
2026-01-12 13:56:11 +0100

    feature: dynamic logo path in class

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from "@angular/common";
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [NgOptimizedImage]
 })
 export class App {
   title = 'Carpediem Restaurant';
+  logoPath = 'assets/carpediem-food-logo.png';
 }
