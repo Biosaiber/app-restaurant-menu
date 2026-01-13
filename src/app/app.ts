26cc238363ba886924a8538dd4e715ace644143f
isubfox <samuel.liska@gmail.com>
2026-01-13 13:17:19 +0100

    feature: connection components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from "@angular/common";
+import { RestaurantMenu } from './restaurant-menu/restaurant-menu';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [NgOptimizedImage]
+  imports: [NgOptimizedImage, RestaurantMenu]
 })
 export class App {
-  title = 'Carpediem Restaurant';
+  title = 'Carpediem Food';
   logoPath = 'assets/carpediem-food-logo.png';
 }
