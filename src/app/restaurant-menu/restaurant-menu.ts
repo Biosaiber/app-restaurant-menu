d0d982140e83d72d236c5aa8761885dd47569580
isubfox <samuel.liska@gmail.com>
2026-01-13 13:16:32 +0100

    feature: data of dishes

--- src/app/restaurant-menu/restaurant-menu.ts
+++ src/app/restaurant-menu/restaurant-menu.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './restaurant-menu.css',
 })
 export class RestaurantMenu {
-
+  dishes = [
+    { name: 'Pizza Margherita', description: 'Classic pizza with tomatoes, mozzarella, and basil.', imageUrl: 'public/margherita-pizza.jpg' },
+    { name: 'Grilled Salmo', description: 'Grilled salmon with lemon and dill.', imageUrl: 'public/grilled-salmon.jpg' }, 
+    { name: 'Baked Lasagna', description: 'Creamy pasta with pancetta, eggs, and cheese.', imageUrl: 'public/baked-lasagna.jpg' }
+  ];
 }
