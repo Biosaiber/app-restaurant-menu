5fdd8749c8ca8996d158fc1cfbb39c5f2b432fec
isubfox <samuel.liska@gmail.com>
2026-01-14 14:05:42 +0100

    feature: component merge and start works together

--- src/app/restaurant-menu/restaurant-menu.ts
+++ src/app/restaurant-menu/restaurant-menu.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
+import { DishRecipe } from '../dish-recipe/dish-recipe';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-restaurant-menu',
-  imports: [],
+  imports: [ DishRecipe,NgOptimizedImage ],
   templateUrl: './restaurant-menu.html',
   styleUrl: './restaurant-menu.css',
 })
 export class RestaurantMenu {
   dishes = [
-    { name: 'Pizza Margherita', description: 'Classic pizza with tomatoes, mozzarella, and basil.', imageUrl: 'public/margherita-pizza.jpg' },
-    { name: 'Grilled Salmo', description: 'Grilled salmon with lemon and dill.', imageUrl: 'public/grilled-salmon.jpg' }, 
-    { name: 'Baked Lasagna', description: 'Creamy pasta with pancetta, eggs, and cheese.', imageUrl: 'public/baked-lasagna.jpg' }
+    { name: 'Pizza Margherita', description: 'Classic pizza with tomatoes, mozzarella, and basil.', imageUrl: '/assets/margherita-pizza.jpg' },
+    { name: 'Grilled Salmo', description: 'Grilled salmon with lemon and dill.', imageUrl: '/assets/grilled-salmon.jpg' }, 
+    { name: 'Baked Lasagna', description: 'Creamy pasta with pancetta, eggs, and cheese.', imageUrl: '/assets/baked-lasagna.jpg' }
   ];
 }
